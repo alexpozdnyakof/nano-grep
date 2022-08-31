b93c34953f82edb97f07da2eb773db41550593fa
alexpozdnyakof <alexpozdnyakof@gmail.com>
2022-09-01 04:48:11 +0700

    feat: add case sensitive and insensitive search

---
+++ src/lib.rs
@@ -0,0 +1,86 @@
+use std::error::Error;
+use std::fs;
+
+pub struct Config {
+  query: String,
+  file_path: String,
+}
+
+impl Config {
+  pub fn build(args: &[String]) -> Result<Config, &'static str> {
+      if args.len() < 3 {
+          return Err("not enough arguments");
+      }
+      let query = args[1].clone();
+      let file_path = args[2].clone();
+
+      Ok(Config { query, file_path})
+  }
+}
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>>{
+  let contents = fs::read_to_string(config.file_path)?;
+
+  println!("With text:\n{contents}");
+  Ok(())
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+  let mut results = Vec::new();
+
+  for line in contents.lines(){
+    if line.contains(query) {
+      results.push(line);
+    }
+  }
+
+  results
+}
+
+pub fn search_case_insensitive<'a>(
+  query: &str,
+  contents: &'a str,
+) -> Vec<&'a str> {
+  let query = query.to_lowercase();
+  let mut results = Vec::new();
+
+  for line in contents.lines() {
+    if line.to_lowercase().contains(&query) {
+      results.push(line);
+    }
+  }
+
+  results
+}
+
+
+#[cfg(test)]
+mod test {
+  use super::*;
+
+  #[test]
+  fn one_result() {
+    let query = "duct";
+    let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Duct tape.";
+
+    assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+  }
+  #[test]
+  fn case_insensitive() {
+      let query = "rUsT";
+      let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+      assert_eq!(
+          vec!["Rust:", "Trust me."],
+          search_case_insensitive(query, contents)
+      );
+  }
+}
