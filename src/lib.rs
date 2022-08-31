25584eadc14cb683b943137560e5d25a8c55c96a
alexpozdnyakof <alexpozdnyakof@gmail.com>
2022-09-01 05:01:23 +0700

    feat: lib run integration

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,10 @@
 use std::error::Error;
 use std::fs;
-
+use std::env;
 pub struct Config {
-  query: String,
-  file_path: String,
+  pub query: String,
+  pub file_path: String,
+  pub ignore_case: bool,
 }
 
 impl Config {
@@ -14,14 +15,29 @@ impl Config {
       let query = args[1].clone();
       let file_path = args[2].clone();
 
-      Ok(Config { query, file_path})
+      let ignore_case = env::var("IGNORE_CASE").is_ok();
+
+      Ok(Config {
+        query,
+        file_path,
+        ignore_case
+      })
   }
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>>{
   let contents = fs::read_to_string(config.file_path)?;
 
-  println!("With text:\n{contents}");
+  let results = if config.ignore_case {
+    search_case_insensitive(&config.query, &contents)
+  } else {
+    search(&config.query, &contents)
+  };
+
+  for line in results {
+    println!("{line}");
+  }
+
   Ok(())
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,22 @@
 use std::env;
-use std::error::Error;
 use std::process;
-
 use nanogrep::Config;
+
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::build(&args);
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
 
-    // println!("Searching for {}", config.query);
-    // println!("In file {}", config.file_path);
 
-    // if let Err(e) = run(config) {
-    //     println!("Application error: {e}");
+    if let Err(e) = nanogrep::run(config) {
+        eprintln!("Application error: {e}");
 
-    //     process::exit(1);
-    // }
+        process::exit(1);
+    }
 }
 
 
